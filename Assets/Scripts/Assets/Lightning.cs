ea076de8c6fc783ea13235e6888fcec81f40b3b3
Jack Gill <49733871+MarioBoy148@users.noreply.github.com>
2022-11-09 10:39:49 -0500

    Boycotting lighting circle

--- Assets/Scripts/Assets/Lightning.cs
+++ Assets/Scripts/Assets/Lightning.cs
@@ -1,15 +1,14 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Lightning : MonoBehaviour
 {
-    public int damage = 2;
+    public int damage = 1;
     public int speed = 20;
     bool canCollide = false;
-    bool canCircle = false;
-    public Animator anim;
-    public CircleCollider2D CircleCol;
+
     private void Awake() // Setting Force Direction When it enters the scene
     {
         StartCoroutine(WaitToCollide());
@@ -23,38 +22,23 @@ public class Lightning : MonoBehaviour
             gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.left * speed, ForceMode2D.Impulse);
         // Starts coroutine to k*ll itself
         StartCoroutine(WaitToDestroy());
-        gameObject.GetComponent<CircleCollider2D>().enabled = false;
-        anim.SetBool("CanCollide", true);
     }
-    private void OnCollisionEnter2D(Collision2D collision) // If it hits an enemy, deal damage
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (canCollide)
         {
             if (collision.transform.tag == "Enemy")
             {
-                Debug.Log("Lmao11");
+                Debug.Log("Direct");
                 collision.gameObject.GetComponent<EnemyTarget>().TakeDamage(damage);
                 canCollide = false;
-                canCircle = true;
                 StartCoroutine(WaitToDestroy(0.2f));
-                CircleCol.enabled = true;
             }
             else
             {
                 StartCoroutine(WaitToDestroy(0f));
             }
-        }
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        // canCircle is confused when multiple enemies can be hit, becomes false after just 1 hit
-        if (collision.tag == "Enemy" && canCircle)
-        {
-            Debug.Log("Lmao11");
-            anim.SetBool("CanCollide", false);
-            collision.gameObject.GetComponent<EnemyTarget>().TakeDamage(damage);
-            canCircle = false;
         }
     }
 
@@ -74,13 +58,5 @@ public class Lightning : MonoBehaviour
     {
         yield return new WaitForSeconds(.05f);
         canCollide = !canCollide;
-    }
-
-    private void Update()
-    {
-        if (anim.GetBool("CanCollide") == false)
-        {
-            anim.Play("LightningCircle");
-        }
     }
 }
