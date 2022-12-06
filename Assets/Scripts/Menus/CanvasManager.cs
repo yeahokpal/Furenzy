4df1334c4d8b09c19be74754b1c8361184802a0d
Jack Gill <49733871+MarioBoy148@users.noreply.github.com>
2022-12-05 19:37:38 -0500

    Slightly modifying Sprites

--- Assets/Scripts/Menus/CanvasManager.cs
+++ Assets/Scripts/Menus/CanvasManager.cs
@@ -9,10 +9,12 @@ public class CanvasManager : MonoBehaviour
     public bool isPaused = false;
     public GameObject pauseMenu;
     public GameObject win;
+    public SaveSystem saveManager;
 
     private void Start()
     {
         Resume();
+        saveManager = GameObject.Find("SaveManager").GetComponent<SaveSystem>();
     }
 
     private void Update()
@@ -43,6 +45,36 @@ public class CanvasManager : MonoBehaviour
     public void Quit()
     {
         SceneManager.LoadScene("MainMenu");
+
         // Add saving to the file
+
+        #region Characters
+        if (GameObject.Find("Fox(Clone)"))
+        {
+            FoxManager fox = GameObject.Find("Fox(Clone)").GetComponent<FoxManager>();
+            saveManager.Write("Player", "health", 1, fox.Health.ToString());
+            saveManager.Write("Player", "mana", 1, fox.mana.ToString());
+        }
+        if (GameObject.Find("Bunny(Clone)"))
+        {
+            BunnyManager bunny = GameObject.Find("Bunny(Clone)").GetComponent<BunnyManager>();
+            saveManager.Write("Player", "health", 1, bunny.Health.ToString());
+            saveManager.Write("Player", "mana", 1, bunny.mana.ToString());
+        }
+        if (GameObject.Find("Bird(Clone)"))
+        {
+            BirdManager bird = GameObject.Find("Bird(Clone)").GetComponent<BirdManager>();
+            saveManager.Write("Player", "health", 1, bird.Health.ToString());
+            saveManager.Write("Player", "mana", 1, bird.mana.ToString());
+        }
+        if (GameObject.Find("Ferret(Clone)"))
+        {
+            FerretManager ferret = GameObject.Find("Ferret(Clone)").GetComponent<FerretManager>();
+            saveManager.Write("Player", "health", 1, ferret.Health.ToString());
+            saveManager.Write("Player", "mana", 1, ferret.mana.ToString());
+        }
+        #endregion
+
+
     }
 }
